f85c6d1e9e0453d63e287b8ea0f912093eac55a3
DavidPiper94 <david.piper@udo.edu>
2016-04-19 16:53:44 +0200

    Initialer Commit des CocoaPod-Projekts

---
+++ Example/Pods/Target Support Files/MetaheuristicKit/MetaheuristicKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MetaheuristicKit : NSObject
+@end
+@implementation PodsDummy_MetaheuristicKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-MetaheuristicKit_Example/Pods-MetaheuristicKit_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_MetaheuristicKit_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_MetaheuristicKit_ExampleVersionString[];
+
