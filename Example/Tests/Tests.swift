9945115fd3221186ebd6b91ae64ff1d24bf58623
DavidPiper94 <david.piper@udo.edu>
2016-05-12 23:42:31 +0200

    weitere Tests implementiert

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -8,6 +8,56 @@ class Tests: XCTestCase {
         
     var GAFloat = GeneticAlgorithm(type: .Float, fitnessFunction: (GeneticAlgorithm.simpleFloatFitnessFunction))
     
+    func testGenerateRandomBitArray() {
+        GABool.sizeOfIndividual = 3
+        let randomBitArray = GABool.generateRandomBitArray()
+        
+        XCTAssertTrue(randomBitArray.count == GABool.sizeOfIndividual)
+    }
+    
+    func testGenerateRandomFloatArray() {
+        GAFloat.sizeOfIndividual = 3
+        let randomFloatArray = GAFloat.generateRandomFloatArray()
+        
+        XCTAssertTrue(randomFloatArray.count == GAFloat.sizeOfIndividual)
+        XCTAssertTrue(randomFloatArray[0] >= GAFloat.minFloatValue && randomFloatArray[0] <= GAFloat.maxFloatValue)
+        XCTAssertTrue(randomFloatArray[1] >= GAFloat.minFloatValue && randomFloatArray[1] <= GAFloat.maxFloatValue)
+        XCTAssertTrue(randomFloatArray[2] >= GAFloat.minFloatValue && randomFloatArray[2] <= GAFloat.maxFloatValue)
+    }
+    
+    func testSimpleBoolFitnessFunction() {
+        let boolArrayA = [true, true, true]
+        let boolArrayB = [false, false, false]
+        
+        XCTAssertTrue(GABool.assessFitness(individual: boolArrayA) == 3)
+        XCTAssertTrue(GABool.assessFitness(individual: boolArrayB) == 0)
+    }
+    
+    func testSimpleFloatFitnessFunction() {
+        let floatArray = [0.1, 0.2, 0.3]
+        
+        //XCTAssertTrue(GAFloat.assessFitness(individual: floatArray) == 0.6)
+    }
+    
+    func testBitFlipMutation() {
+        let individual = [true, true, false, true]
+        
+        GABool.probabilityOfMutation = 0.0
+        var newIndividual = GABool.bitFlipMutation(individual: individual)
+        
+        XCTAssertTrue(individual[0] == newIndividual[0])
+        XCTAssertTrue(individual[1] == newIndividual[1])
+        XCTAssertTrue(individual[2] == newIndividual[2])
+        XCTAssertTrue(individual[3] == newIndividual[3])
+        
+        GABool.probabilityOfMutation = 1.0
+        newIndividual = GABool.bitFlipMutation(individual: individual)
+        
+        XCTAssertTrue(individual[0] != newIndividual[0])
+        XCTAssertTrue(individual[1] != newIndividual[1])
+        XCTAssertTrue(individual[2] != newIndividual[2])
+        XCTAssertTrue(individual[3] != newIndividual[3])
+    }
     
     func testRandomFloatInRange() {
         let random = GABool.randomFloatInRange(firstNum: 0.0, secondNum: 1.0)
@@ -31,5 +81,13 @@ class Tests: XCTestCase {
         
         XCTAssertTrue(GABool.containsOnlyZeros(array: arrayWithOnlyZeros))
         XCTAssertFalse(GABool.containsOnlyZeros(array: arrayWithNotOnlyZeros))
+    }
+    
+    func testRandomVectorShuffle() {
+        var vector = [1,2,3,4,5]
+        
+        //GABool.randomVectorShuffle(&vector)
+        
+        XCTAssertTrue(vector.count == 5)
     }
 }
